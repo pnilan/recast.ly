9bfcc38f310244735f1415d9045909ae2213c308
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-30 18:24:20 -0700

    App, VideoList, VideoListEntry, and VideoPlayer render correctly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,48 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import exampleVideoData from '../data/exampleVideoData.js';
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+// import React, { useState } from 'react';
+
+// App
+// || => Search
+// || => VideoPlayer
+// || => VideoList
+//         || => VideoListEntry
+//
+
+// import VideoList from './VideoList';
+var App = () => {
+
+  const [video, setVideo] = useState(exampleVideoData[0]);
+
+  // 1. Set up useState hook for video to pass
+  // down to VideoPlayer component
+  // 2. Create 'play video' function that
+  // invokes setVideo with clicked video
+  // list entry
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <Search />
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer video={video}/>
+
+        </div>
+        <div className="col-md-5">
+          {/* changed this */}
+          <VideoList videos={exampleVideoData} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,18 @@
-var VideoList = () => (
+
+
+import VideoListEntry from './videoListEntry.js';
+
+
+var VideoList = ({ videos }) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+
+    {videos.map((video) => (
+      <VideoListEntry key={video.id.videoId} video={video} />
+    ))}
+
+    {/* {videos.forEach((video) => (
+      <VideoListEntry key={video.id.videoId} video={video} />
+    ))} */}
   </div>
 );
 
